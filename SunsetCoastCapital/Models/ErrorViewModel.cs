3e7db003b61b339307d5fbf39cb445578a65febf
JacqThomas <jacqueline.thomas@sunsetcoast.capital>
2023-06-27 17:25:14 -0400

    Add project files.

---
+++ SunsetCoastCapital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SunsetCoastCapital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
